452a7437121f645b3e8b8c2f3bcfac78c84f2d4b
ffredkz <dx.ddt.18@gmail.com>
2025-07-24 20:33:13 -0600

    Add files via upload

---
+++ love letter 2.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  var envelope = $("#envelope");
+  var btn_open = $("#open");
+  var btn_reset = $("#reset");
+
+  envelope.click(function () {
+    open();
+  });
+  btn_open.click(function () {
+    open();
+  });
+  btn_reset.click(function () {
+    close();
+  });
+
+  function open() {
+    envelope.addClass("open").removeClass("close");
+    envelope.addClass("open").removeClass("inicio")
+  }
+  function close() {
+    envelope.addClass("close").removeClass("open");
+    envelope.addClass("close").removeClass("inicio")
+  }
+});
